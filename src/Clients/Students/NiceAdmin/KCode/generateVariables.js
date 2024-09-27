51b320137b8a75b366e89494c4911af7e482eebd
keshavsoft <nknnkumar@yahoo.com>
2024-09-27 18:01:26 +0530

    good

--- src/Clients/Students/NiceAdmin/KCode/generateVariables.js
+++ src/Clients/Students/NiceAdmin/KCode/generateVariables.js
@@ -3,21 +3,18 @@
 // import { StartFunc as GetTableNames } from "./GetTableNames.js";
 // import { StartFunc as ReadDataSchema } from "./ReadDataSchema.js";
 
-import { StartFunc as prepareSideBar } from "./prepareSideBar.js";
+// import { StartFunc as prepareSideBar } from "./prepareSideBar.js";
+// import sidebarItems from './sideBarItems.json' with {type: 'json'};
 
-const StartFunc = ({ mode, inFilesArray, inSrcPath }) => {
+const StartFunc = ({ mode, inFilesArray }) => {
     const variables = {};
     let LocalFiles = inFilesArray;
-    const sidebarItems = prepareSideBar();
 
     Object.keys(LocalFiles).forEach((filename) => {
         variables[filename + '.html'] = {
             web_title: "Mazer Admin Dashboard",
             filename: filename + '.html',
-            sidebarItems,
-            isDev: mode === 'development',
-            tableName: "",
-            columnData: {}
+            isDev: mode === 'development'
         };
     });
 
